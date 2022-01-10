0c4d4ec37d43ca705bac336ee439ca0d69d0a032
DEAMBROGGI <adrian.m.deambroggi@gmail.com>
2022-01-10 14:08:36 -0300

    Handling, Tax y Shipping

--- src/components/PayForm/PayPal.js
+++ src/components/PayForm/PayPal.js
@@ -33,7 +33,7 @@ export default function PayPal() {
        //Creo la orden de con los datos, esta puede ser general o con detalle de items
       console.log(data)
     return actions.order.create({
-             purchase_units: [
+            /* purchase_units: [
        {
           description:"items",
           amount: {
@@ -43,24 +43,23 @@ export default function PayPal() {
         },
      
        
-      ],
+      ],*/
  
-     /* purchase_units: [
-        {
-            reference_id: "PUHF",
-            description: "Some description",
+     purchase_units: [{
+                reference_id: "PUHF",
+                description: "Sporting Goods",
 
-            custom_id: "Something7364",
-            soft_descriptor: "Great description 1",
-            amount: {
-                currency_code: "USD",
-                value: "200.00",
-                breakdown: {
-                    item_total: {
-                        currency_code: "USD",
-                        value: "200.00"
-                    }
-                    shipping: {
+                custom_id: "CUST-HighFashions",
+                soft_descriptor: "HighFashions",
+                amount: {
+                    currency_code: "USD",
+                    value: "230.00",
+                    breakdown: {
+                        item_total: {
+                            currency_code: "USD",
+                            value: "180.00"
+                        },
+                        shipping: {
                             currency_code: "USD",
                             value: "30.00"
                         },
@@ -76,33 +75,55 @@ export default function PayPal() {
                             currency_code: "USD",
                             value: "10"
                         }
-                }
-            },
-            items: [
-                {
-                    name: "Item 1",
-                    description: "The best item ever",
-                    sku: "xyz-2654",
-                    unit_amount: {
-                        currency_code: "USD",
-                        value: "100.00"
-                    },
-                    quantity: "1"
+                    }
                 },
-                {
-                    name: "Item 2",
-                    description: "Not bad too",
-                    sku: "zdc-3942",
+                items: [{
+                    name: "T-Shirt",
+                    description: "Green XL",
+                    sku: "sku01",
                     unit_amount: {
-                        currency_code: "USD",
-                        value: "50.00"
+                         currency_code: "USD",
+                         value: "90.00"
                     },
-                    quantity: "2"
+                    tax: {
+                        currency_code: "USD",
+                        value: "10.00"
+                    },
+                    quantity: "1",
+                    category: "PHYSICAL_GOODS"
+                },
+                    {
+                    name: "Shoes",
+                    description: "Running, Size 10.5",
+                    sku: "sku02",
+                    unit_amount: {
+                         currency_code: "USD",
+                         value: "45.00"
+                    },
+                    tax: {
+                        currency_code: "USD",
+                        value: "5.00"
+                    },
+                    quantity: "2",
+                    category: "PHYSICAL_GOODS"
                 }
-            ],
-
-        }
-    ]*/
+                ],
+                shipping: {
+                    method: "United States Postal Service",
+                    address: {
+                        name: {
+                            full_name: "John",
+                            surname: "Doe"
+                        },
+                        address_line_1: "123 Townsend St",
+                        address_line_2: "Floor 6",
+                        admin_area_2: "San Francisco",
+                        admin_area_1: "CA",
+                        postal_code: "94107",
+                        country_code: "US"
+                    }
+                }
+            }]
     });
   };
   const onApprove = (data, actions) => { //recibo el resultado de mi operacion
